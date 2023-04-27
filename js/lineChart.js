57e6face48d8185caa974e9a03bb2eb6431ceaaf
Carlos Alvarez del Castillo Saleh <carlosadelcs@gmail.com>
2023-04-27 10:18:34 -0500

    Added linechart to index

--- js/lineChart.js
+++ js/lineChart.js
@@ -48,56 +48,6 @@ d3.json(pathToJsonData)
         monthDropdown.property('selectedIndex', 0);
     }
 
-    // Create climate indicators to fill dropdown
-    const climateIndicators = [
-        'Sea Ice',
-        'Snow Cover'
-    ]
-
-    // Create a dictionary to easily convert from label to value format
-    const climateIndicatorLabelToValue = {
-        'Sea Ice'   : 'seaIce',
-        'Snow Cover': 'snowCover'
-    }
-
-    // Select the dropdown element and populate it with months
-    const climateIndicatorDropdown = d3.select('#climateIndicator-select2');
-    climateIndicatorDropdown.selectAll('option')
-        .data(climateIndicators)
-        .enter()
-        .append('option')
-        .text(d => d)
-        .attr('value', d => d);
-
-    // Set the first dropdown option as the default (if there are options)
-    if (climateIndicatorDropdown.select('option').size() > 0) {
-        climateIndicatorDropdown.property('selectedIndex', 0);
-    }
-
-    // create regions to fill dropdown
-    const regions = [
-        'Northern Hemisphere',
-        'Southern Hemisphere'
-    ];
-
-    const regionLabelToValue = {
-        'Northern Hemisphere' : 'northernHemisphere',
-        'Southern Hemisphere' : 'southernHemisphere'
-    };
-
-    const regionDropdown = d3.select('#region-select2');
-    regionDropdown.selectAll('option')
-        .data(regions)
-        .enter()
-        .append('option')
-        .text(d => d)
-        .attr('value', d => d);
-
-    // Set the first dropdown option as the default
-    if (regionDropdown.select('option').size() > 0) {
-        regionDropdown.property('selectedIndex', 0);
-    }
-
     // Create SVG container
     const margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
@@ -117,17 +67,14 @@ d3.json(pathToJsonData)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     // Define the function to update the chart
-    function updateLinechart(selectedMonth, selectedClimateIndicator, selectedRegion) {
+    function updateLinechart(selectedMonth) {
         // Get the new relevant data to plot based on new filter
         const filteredData1 = Object.values(contents).filter(d => {
             monthNum = monthNameToNum[selectedMonth];
-            climateIndicatorValue = climateIndicatorLabelToValue[selectedClimateIndicator];
-            regionValue = regionLabelToValue[selectedRegion];
             return d.month === monthNum && d.climateIndicator === 'snowCover' && d.region === "northAmerica"});
 
         const filteredData2 = Object.values(contents).filter(d => {
             monthNum = monthNameToNum[selectedMonth];
-            regionValue = regionLabelToValue[selectedRegion];
             return d.month === monthNum && d.climateIndicator === 'seaIce' && d.region === "northernHemisphere"});
             console.log(filteredData2);
 
@@ -181,33 +128,13 @@ d3.json(pathToJsonData)
         }
 
     // Initialize the chart with the default month and indicator
-    updateLinechart(monthDropdown.property('value'), climateIndicatorDropdown.property('value'));
+    updateLinechart(monthDropdown.property('value'), );
 
     // Add an event listener to the month selector dropdown
     // this makes it so when the dropdown values change the graph is updated
     monthDropdown.on('change', function() {
         const selectedMonth = d3.select(this).property('value');
-        const selectedClimateIndicator = d3.select("#climateIndicator-select2").property('value');
-        const selectedRegion = d3.select('#region-select2').property('value');
-        updateLinechart(selectedMonth, selectedClimateIndicator, selectedRegion);
-    });
-
-    // Add an event listener to the climate indicator selector dropdown
-    // this makes it so when the dropdown values change the graoh is updated
-    climateIndicatorDropdown.on('change', function() {
-        const selectedMonth = d3.select('#month-select2').property('value');
-        const selectedClimateIndicator = d3.select(this).property('value');
-        const selectedRegion = d3.select('#region-select2').property('value');
-        updateLinechart(selectedMonth, selectedClimateIndicator, selectedRegion);
-    });
-
-    // Add an event listener to the climate indicator selector dropdown
-    // this makes it so when the dropdown values change the graoh is updated
-    regionDropdown.on('change', function() {
-        const selectedMonth = d3.select('#month-select2').property('value');
-        const selectedClimateIndicator = d3.select("#climateIndicator-select2").property('value');
-        const selectedRegion = d3.select(this).property('value');
-        updateLinechart(selectedMonth, selectedClimateIndicator, selectedRegion);
+        updateLinechart(selectedMonth);
     });
 })
 .catch(error => {
