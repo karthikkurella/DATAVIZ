6ab64a2924e3a569ef3a6ae7d9b9d6913b20c841
Carlos Alvarez del Castillo Saleh <carlosadelcs@gmail.com>
2023-05-01 22:02:04 -0500

    Made comments prettier

--- js/barchartTemplate.js
+++ js/barchartTemplate.js
@@ -1,7 +1,7 @@
 {
   // Months dropdown ///////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////
-  // Create months to fill dropdown
+  // Create months to fill dropdown.
   const monthLabels = [
     'January', 
     'February', 
@@ -17,7 +17,7 @@
     'December'
   ];
 
-  // Create a dictionary to easily convert from name to number format
+  // Create a dictionary to easily convert from name to number format.
   const monthLabelToValue = {
     'January'   : '01',
     'February'  : '02',
@@ -33,7 +33,7 @@
     'December'  : '12'
   }
 
-  // Select the dropdown element and populate it with months
+  // Select the dropdown element and populate it with months.
   const monthDropdown = d3.select('#month-select');
   monthDropdown.selectAll('option')
     .data(monthLabels)
@@ -42,26 +42,26 @@
     .text(d => d)
     .attr('value', d => d);
 
-  // Set the first dropdown option as the default (if there are options)
+  // Set the first dropdown option as the default (if there are options).
   if (monthDropdown.select('option').size() > 0) {
     monthDropdown.property('selectedIndex', 0);
   }
 
   // Climate indicators dropdown ///////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////
-  // Create climate indicators to fill dropdown
+  // Create climate indicators to fill dropdown.
   const climateIndicatorLabels = [
     'Sea Ice',
     'Snow Cover'
   ]
 
-  // Create a dictionary to easily convert from label to value format
+  // Create a dictionary to easily convert from label to value format.
   const climateIndicatorLabelToValue = {
     'Sea Ice'   : 'seaIce',
     'Snow Cover': 'snowCover'
   }
 
-  // Select the dropdown element and populate it with climate indicators
+  // Select the dropdown element and populate it with climate indicators.
   const climateIndicatorDropdown = d3.select('#climateIndicator-select');
   climateIndicatorDropdown.selectAll('option')
     .data(climateIndicatorLabels)
@@ -70,21 +70,21 @@
     .text(d => d)
     .attr('value', d => d);
 
-  // Set the first dropdown option as the default (if there are options)
+  // Set the first dropdown option as the default (if there are options).
   if (climateIndicatorDropdown.select('option').size() > 0) {
     climateIndicatorDropdown.property('selectedIndex', 0);
   }
 
   // Region dropdown ///////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////
-  // Create climate indicators to fill dropdown when sea ice is selected
+  // Create climate indicators to fill dropdown when sea ice is selected.
   const seaIceRegionLabels = [
     'Global',
     'Northern Hemisphere',
     'Southern Hemisphere'
   ]
 
-  // Create climate indicators to fill dropdown when snoew cover is selected
+  // Create climate indicators to fill dropdown when snoew cover is selected.
   const snowCoverRegionLabels = [
     'Eurasia',
     'North America',
@@ -92,7 +92,7 @@
     'Northern Hemisphere'
   ]
 
-  // Create a dictionary to easily convert from label to value format
+  // Create a dictionary to easily convert from label to value format.
   const regionLabelToValue = {
     'Global'   : 'global',
     'Northern Hemisphere': 'northernHemisphere',
@@ -108,9 +108,9 @@
   // is available for a specific region. It also sets the first element of
   // the dropdown as its value.
   function updateRegionDropdown() {
-    // Get the value of the climate indicator dropdown
+    // Get the value of the climate indicator dropdown.
     const selectedClimateIndicator = d3.select('#climateIndicator-select').property('value');
-    // Figure out which labels to use from that value
+    // Figure out which labels to use from that value.
     let regionLabels = seaIceRegionLabels; // use seaIceLabels by default
     if(selectedClimateIndicator == 'Sea Ice') {
       regionLabels = seaIceRegionLabels;
@@ -118,13 +118,13 @@
       regionLabels = snowCoverRegionLabels;
     }
 
-    // Select the region dropdown
+    // Select the region dropdown.
     const regionDropdown = d3.select('#region-select');
 
-    // Remove existing labels
+    // Remove existing labels.
     regionDropdown.selectAll('option').remove();
     
-    // Add new labels
+    // Add new labels.
     regionDropdown.selectAll('option')
     .data(regionLabels)
     .enter()
@@ -132,31 +132,31 @@
     .text(d => d)
     .attr('value', d => d);
 
-    // Set the first dropdown option as the default (if there are options)
+    // Set the first dropdown option as the default (if there are options).
     if (regionDropdown.select('option').size() > 0) {
       regionDropdown.property('selectedIndex', 0);
     }
   }
 
-  // Select the dropdown for easy access later in script
+  // Select the dropdown for easy access later in script.
   const regionDropdown = d3.select('#region-select');
 
-  // Call the function to initialize the dropdown
+  // Call the function to initialize the dropdown.
   updateRegionDropdown();
 
   // Chart /////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////
-  // Define the path to the JSON file
+  // Define the path to the JSON file.
   const pathToJsonData = '../data/all_data.json';
 
-  // Use D3 to load the JSON file
+  // Use D3 to load the JSON file.
   d3.json(pathToJsonData)
   .then(contents => {
-    // Print the initial contents of the JSON file to the console
+    // Log the initial contents of the JSON file (useful for debugging).
     // console.log(contents);
 
-    // Create the svg chart container element
-    // This will hold the bars and axes for the barchart
+    // Create the svg chart container element.
+    // This will hold the bars and axes for the barchart.
     const margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 1200 - margin.left - margin.right,
     height = 775 - margin.top - margin.bottom;
@@ -174,11 +174,11 @@
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Append a new element to hold the y-axis label
+    // Append a new element to hold the y-axis label.
     const yAxisLabelElement = svg.append('g')
       .attr('class', 'y-axis-label');
 
-    // Append text to y-axis label
+    // Append text to y-axis label.
     const yAxisLabelText = yAxisLabelElement.append('text')
       .attr('transform', 'rotate(-90)')
       .attr('y', margin.left - 90)
@@ -186,45 +186,45 @@
       .attr('dy', '1em')
       .text('Million Kilometers Squared');
 
-    // Append a new element to hold the title
+    // Append a new element to hold the title.
     const chartTitleElement = svg.append('g')
       .attr('class', 'chart-title');
 
-    // Append text to chart title
+    // Append text to chart title.
     const chartTitleText = chartTitleElement.append('text')
       .attr('x', width / 2)
       .attr('y', margin.top / 2)
       .attr('text-anchor', 'middle')
       .text(``); // default title text is nothing
 
-    // Define the function to update the chart title
-    // The title should include the region and climate indicator
+    // Define the function to update the chart title.
+    // The title should include the region and climate indicator.
     function updateChartTitle(selectedRegion, selectedClimateIndicator) {
       const newTitleText = `${selectedRegion} ${selectedClimateIndicator}`
       chartTitleText.text(newTitleText);
     }
 
-    // Define the function to update the chart
+    // Define the function to update the chart.
     function updateChart(selectedMonth, selectedClimateIndicator, selectedRegion) {
       updateChartTitle(selectedRegion, selectedClimateIndicator);
       monthValue = monthLabelToValue[selectedMonth];
       climateIndicatorValue = climateIndicatorLabelToValue[selectedClimateIndicator];
       regionValue = regionLabelToValue[selectedRegion];
-      // Get new relevant data to plot based on new filter
+      // Get new relevant data to plot based on new filter.
       const filteredData = Object.values(contents).filter(d => {
         return (d.month === monthValue 
                 && d.climateIndicator === climateIndicatorValue 
                 && d.region === regionValue)
         }
       );
-      // log the filtered data
+      // Log the filtered data (useful for debugging).
       // console.log(filteredData);
 
-      // change the x and y domains to fit the new data
+      // change the x and y domains to fit the new data.
       x.domain(filteredData.map(d => d.year));
       y.domain([0, d3.max(filteredData, d => d.value)]);
 
-      // replace all bars with the ones for the new data
+      // replace all bars with the ones for the new data.
       svg.selectAll('.bar').remove();
       svg.selectAll('.bar')
         .data(filteredData)
@@ -235,7 +235,7 @@
         .attr('y', d => y(d.value))
         .attr('height', d => height - y(d.value));
 
-      // replace all axis with the ones for the new data
+      // replace all axis with the ones for the new data.
       svg.selectAll('.axis').remove();
       svg.append('g')
         .attr('class', 'axis axis-x')
@@ -246,13 +246,13 @@
         .call(d3.axisLeft(y));
     }
 
-    // Initialize the chart with the default month and indicator
+    // Initialize the chart with the default month and indicator.
     updateChart(monthDropdown.property('value'), 
                 climateIndicatorDropdown.property('value'), 
                 regionDropdown.property('value'));
 
-    // Add an event listener to the month selector dropdown
-    // This makes it so when the dropdown values change the graph is updated
+    // Add an event listener to the month selector dropdown.
+    // This makes it so when the dropdown values change the graph is updated.
     monthDropdown.on('change', function() {
       const selectedMonth = d3.select(this).property('value');
       const selectedClimateIndicator = d3.select('#climateIndicator-select').property('value');
@@ -260,9 +260,10 @@
       updateChart(selectedMonth, selectedClimateIndicator, selectedRegion);
     });
 
-    // Add an event listener to the climate indicator selector dropdown
-    // This makes it so when the dropdown values change the graph is updated
-    // It also updates the values in the region dropdown
+    // Add an event listener to the climate indicator selector dropdown.
+    // This makes it so when the dropdown values change the graph is updated.
+    // It also updates the values in the region dropdown. This is necessary because
+    // the data we have has different regions for each climate indicator.
     climateIndicatorDropdown.on('change', function() {
       const selectedMonth = d3.select('#month-select').property('value');
       const selectedClimateIndicator = d3.select(this).property('value');
@@ -271,8 +272,8 @@
       updateChart(selectedMonth, selectedClimateIndicator, selectedRegion);
     });
 
-    // Add an event listener to the region dropdown
-    // this makes it so when the dropdown values change the graph is updated
+    // Add an event listener to the region dropdown.
+    // this makes it so when the dropdown values change the graph is updated.
     regionDropdown.on('change', function() {
       const selectedMonth = d3.select('#month-select').property('value');
       const selectedClimateIndicator = d3.select('#climateIndicator-select').property('value');
@@ -281,7 +282,7 @@
     });
   })
   .catch(error => {
-    // Handle any errors that occur during loading
+    // Handle any errors that occur during loading.
     console.error(error);
   });
 }
